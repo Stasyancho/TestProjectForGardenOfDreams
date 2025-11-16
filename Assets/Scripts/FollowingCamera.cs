23bfe61b086ebd04a6242148cc1b666d731e1103
Stasyancho <stasyancho@gmail.com>
2025-11-16 14:48:09 +0300

    Added some scripts and resourses
    Added resource packs and scripts for player and camera
    Created tile map

---
+++ Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject _character;
+    [SerializeField] private float _returnSpeed;
+
+    void Start()
+    {
+        transform.position = new Vector3(_character.transform.position.x, _character.transform.position.y, -1);
+    }
+
+    void Update()
+    {
+        CameraMove();
+    }
+
+    void CameraMove()
+    {
+        var currentVector = new Vector3(_character.transform.position.x, _character.transform.position.y, -1);
+        transform.position = Vector3.Lerp(transform.position, currentVector, _returnSpeed * Time.deltaTime);
+    }
+}
+
